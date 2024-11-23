17b853ac7b524296023b092ee3679f906e5f1f5a
far1h <farihmuhammad45@gmail.com>
2024-11-23 20:13:55 +0700

    connect tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,16 @@
-Hello World
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel Job Board</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <div class="text-4xl">This should be a big text!</div>
+</body>
+
+</html>
