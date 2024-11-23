df39355864575f7cdc64ebe6f0214f8023f6ce24
far1h <farihmuhammad45@gmail.com>
2024-11-23 20:23:59 +0700

    Add layouts using components

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
-<?php
+ <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('jobs', JobController::class);
