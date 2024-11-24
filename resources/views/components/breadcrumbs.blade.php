34fb35f9cf1dceffe614357e53d8ebe8560657f3
far1h <farihmuhammad45@gmail.com>
2024-11-24 08:21:03 +0700

    Add Breadcrumbs Navigation

---
+++ resources/views/components/breadcrumbs.blade.php
@@ -0,0 +1,16 @@
+@props(['links'])
+<nav {{ $attributes }}>
+    <ul class="flex items-center space-x-4 text-slate-500">
+        <li>
+            <a href="/">Home</a>
+        </li>
+        @foreach ($links as $label => $link)
+            <li>→</li>
+            <li>
+                <a href="{{ $link }}">
+                    {{ $label }}
+                </a>
+            </li>
+        @endforeach 
+    </ul>
+</nav>
