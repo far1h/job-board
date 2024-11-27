efaf83adb6d8fd6a45b4353e622b854951a2819f
far1h <farihmuhammad45@gmail.com>
2024-11-27 14:24:14 +0700

    Tried using scopes and it didn't work so just clean the routes by moving function to controller

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/suggestions', function () {
-    $filters = request()->only('search', 'min_salary','experience','category');
-    $suggestions = Job::filter($filters)->get();
-    return response()->json($suggestions);
-});
-
 Route::get('', fn() => to_route('jobs.index'));
 Route::resource('jobs', JobController::class);
+Route::get('/jobs/suggestions', [JobController::class, 'JobController@suggestions']);
 
