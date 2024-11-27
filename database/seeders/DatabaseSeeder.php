af67913fd85ec8b6651d36024a61359dea11cc5c
far1h <farihmuhammad45@gmail.com>
2024-11-27 15:02:30 +0700

    Add Employer Factory and Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employer;
 use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,12 +15,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(count: 300)->create();
+        $users = User::all()->shuffle();
+
+        for ($i=0; $i < 20; $i++) {
+            Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+        $employers = Employer::all();
+
+        for ($i=0; $i < 100; $i++) {
+            Job::factory()->create([
+                'employer_id' => $employers->random()->id
+            ]);
+        }
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Job::factory(100)->create();
+
     }
 }
