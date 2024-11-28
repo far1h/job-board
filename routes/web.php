5d5f6dec7747514ddac186349888cd13716fd8cd
far1h <farihmuhammad45@gmail.com>
2024-11-28 19:42:45 +0700

    Add authentication controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
  <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
 use App\Models\Job;
 use Illuminate\Http\Request;
@@ -8,5 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/suggestions', [JobController::class, 'suggestions']);
 Route::get('', fn() => to_route('jobs.index'));
-Route::resource('jobs', JobController::class);
-
+Route::resource('jobs', JobController::class)->only(['index','show']);
+Route::get('login', fn()=> to_route('auth.create'))->name('login');
+Route::resource('auth', AuthController::class)->only(['create','store']);
