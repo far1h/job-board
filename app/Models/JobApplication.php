c0e48acf94254eb3d2d97ea1da13ab95aa337938
far1h <farihmuhammad45@gmail.com>
2024-12-04 15:33:45 +0700

    Add Logic for Job Application

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -10,6 +10,8 @@ class JobApplication extends Model
     /** @use HasFactory<\Database\Factories\JobApplicationFactory> */
     use HasFactory;
 
+    protected $fillable = ['expected_salary', 'user_id', 'job_id'];
+    
     public function job() {
         return $this->belongsTo(Job::class);
     }
