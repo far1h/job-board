06ca70cded6a4bb3403dec38fa052455498c3276
far1h <farihmuhammad45@gmail.com>
2024-12-05 09:11:07 +0700

    Show My Application Details

--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -1,7 +1,19 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" :links="['My Job Applications' => '#']" />
     @forelse ($applications as $application)
-        <x-job-card :job="$application->job"></x-job-card>
+    <x-job-card :job="$application->job">
+        <div class="flex items-center justify-between text-xs text-slate-500">
+          <div>
+            <div>
+              Applied {{ $application->created_at->diffForHumans() }}
+            </div>
+            <div>
+              Your asking salary ${{ number_format($application->expected_salary) }}
+            </div>
+          </div>
+          <div>Right</div>
+        </div>
+      </x-job-card>
     @empty
     @endforelse
 </x-layout>
