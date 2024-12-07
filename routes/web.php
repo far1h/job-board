364a1547e45937a7f50491637e11fa6c7f8e792f
far1h <farihmuhammad45@gmail.com>
2024-12-07 08:11:11 +0700

    Register as an Employer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
  <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobApplicationController;
@@ -20,4 +21,6 @@ Route::middleware('auth')->group(function(){
     Route::resource('job.application', JobApplicationController::class)->only(['create','store']);
     Route::resource('my-job-applications', MyJobApplicationController::class)
     ->only(['index', 'destroy']);
+    Route::resource('employer', EmployerController::class)
+    ->only(['create', 'store']);
 });
