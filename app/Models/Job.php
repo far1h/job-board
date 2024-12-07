2b81e935d3f5be647fba60b4c4a49feacc611def
far1h <farihmuhammad45@gmail.com>
2024-12-07 14:50:58 +0700

    Add Create Jobs Logic

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,6 +12,14 @@ class Job extends Model
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'location',
+        'salary',
+        'description',
+        'experience',
+        'category'
+    ];
     public static array $experience = ['entry', 'intermediate', 'senior'];
     public static array $category = [
         'IT',
