ac5727df2dab4ab65b146b79dcff28cba4e884ff
far1h <farihmuhammad45@gmail.com>
2024-12-08 09:52:14 +0700

    Modify policies

--- app/Policies/JobPolicy.php
+++ app/Policies/JobPolicy.php
@@ -11,7 +11,12 @@ class JobPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(?User $user): bool
+    {
+        return true;
+    }
+
+    public function viewAnyEmployer(User $user): bool
     {
         return true;
     }
@@ -19,7 +24,7 @@ class JobPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Job $job): bool
+    public function view(?User $user, Job $job): bool
     {
         return true;
     }
@@ -29,15 +34,21 @@ class JobPolicy
      */
     public function create(User $user): bool
     {
-        return false;
+        return $user->employer !== null;
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Job $job): bool
+    public function update(User $user, Job $job): bool|Response
     {
-        return false;
+        if ($job->employer->user_id !== $user->id) {
+            return false;
+        }
+        if ($job->jobApplications()->count() > 0) {
+            return Response::deny('Cannot change the job with applications');
+        }
+        return true;
     }
 
     /**
@@ -45,7 +56,7 @@ class JobPolicy
      */
     public function delete(User $user, Job $job): bool
     {
-        return false;
+        return $job->employer->user_id === $user->id;
     }
 
     /**
@@ -53,7 +64,7 @@ class JobPolicy
      */
     public function restore(User $user, Job $job): bool
     {
-        return false;
+        return $job->employer->user_id === $user->id;
     }
 
     /**
@@ -61,7 +72,7 @@ class JobPolicy
      */
     public function forceDelete(User $user, Job $job): bool
     {
-        return false;
+        return $job->employer->user_id === $user->id;
     }
 
     public function apply(User $user, Job $job): bool
